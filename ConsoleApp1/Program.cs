6f0850a3ea891230f80e4b75a85e88d8105621a8
neeraj3161 <neerajtripathi7447@gmail.com>
2022-09-21 19:12:15 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Neeraj";
+            int charAge = 35;
+            char singleChar = 'c';
+            double somenum = 3.55544;
+            Console.WriteLine("Name: "+name + "\nAge: "+charAge + "\nSingle Character: "+singleChar+"\nDouble number: "+somenum);
+            Console.ReadKey();
+        }
+    }
+}
