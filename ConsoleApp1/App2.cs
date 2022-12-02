0b367d25c1ab7895de19ff1d1fb4b7f849f9f30b
neeraj3161 <neerajtripathi7447@gmail.com>
2022-12-02 15:17:32 +0530

    v2

---
+++ ConsoleApp1/App2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class App2
+    {
+        //ValueTypes- int, float, double, structs, enums etc
+        //Reference - Interface, Class, delegates, arrays
+
+        //To make calue types nullable use ?
+
+        static void Main1(String[] args)
+        {
+            int? i = 0;
+            i = null;
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nullableObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace nullableObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ValueTypes- int, float, double, structs, enums etc
+            //Reference - Interface, Class, delegates, arrays
+
+            //To make calue types nullable use ?
+
+            
+                int? i = 0;
+                i = null;
+                Console.WriteLine(i);
+                Console.ReadKey();
+            
+        }
+    }
+}
