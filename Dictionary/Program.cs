c9abb7d76b2b5bd69290c45f78bfe7fd3f64e7f2
neeraj3161 <neerajtripathi7447@gmail.com>
2022-12-02 15:45:37 +0530

    v2

---
+++ Dictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //creating the dictionary
+            Dictionary<string,int> keyValuePairs = new Dictionary<string,int>();
+
+
+            //adding items to the dict
+            //the key in the dictionary should be unique
+
+
+            keyValuePairs.Add("Jonh", 22);
+            keyValuePairs.Add("Mark", 22);
+            keyValuePairs.Add("Albert", 22);
+            keyValuePairs.Add("Mike", 22);
+            keyValuePairs.Add("Micheal", 22);
+            keyValuePairs.Add("Jayson", 22);
+            keyValuePairs.Add("Adam", 22);
+            keyValuePairs.Add("Marshal", 22);
+
+            foreach (KeyValuePair<string, int> pair in keyValuePairs)
+            {
+                //formatted string
+                Console.WriteLine("Value = {0}", pair.Value);
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
--- nullableObjects/Program.cs
+++ nullableObjects/Program.cs
@@ -14,6 +14,32 @@ namespace nullableObjects
             
                 int? i = 0;
                 i = null;
+
+            //same goes for other value types 
+
+            bool? b = true;
+            b = null;
+            if (!b.HasValue) {
+                //HasValue will check if there's any value
+                try
+                {
+                    Console.WriteLine("Boolean type is " + b.Value);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    //We can also throw a custom exception
+                    //this will throw a in windows exception
+                    //throw new ArgumentException("pps we got a error here.", ex);
+
+                }
+            }
+
+            //now we will assign a value to b
+
+            bool c = b ?? true;
+            //the above line says if b is null assign it true
+            Console.WriteLine("Now c is "+c);
                 Console.WriteLine(i);
                 Console.ReadKey();
             
